00910ba494d280308b17abe578f86dc80e8b1491
Yeinalee <yeinalee@gmail.com>
2025-01-08 17:06:03 +0900

    조건문 9~12번 closed #1

---
+++ ConditionalStatement/b10952.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int a, b;
+    cin >> a >> b;
+    while (a != 0 || b != 0)
+    {
+        cout << a + b << '\n';
+        cin >> a >> b;
+    }
+    return 0;
+}
