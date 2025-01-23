605226ed7d996fa5e0487cede1879422b7de1500
Yeinalee <yeinalee@gmail.com>
2025-01-23 14:51:12 +0800

    10807번

--- Array1D/b10807.cpp
+++ Array1D/b10807.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
-
-int main() {
-  int n;
-  int arr[101];
-  int v;
-
-  cin >> n;
-
-  for(int i = 0; i < n; i++) {
-    cin >> arr[i];
-  }
-  cin >> v;
-
-  int res = 0; 
-  for(int i = 0; i < n; i++) {
-    if(arr[i] == v) 
-      res++;
-  }
-
-  cout << res;
+int main()
+{
+    int n, x, v, answer = 0;
+    vector<int> num;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x;
+        num.push_back(x);
+    }
+    cin >> v;
+    for (int i = 0; i < n; i++)
+    {
+        if (v == num[i])
+            answer++;
+    }
+    cout << answer << '\n';
+    return 0;
 }
