265fa2cb2cecf1da1f07d972b4ceb90c55c17daa
Yeinalee <yeinalee@gmail.com>
2025-02-15 17:12:49 +0900

    14889번 시간초과

--- Backtracking/b14889.cpp
+++ Backtracking/b14889.cpp
@@ -2,9 +2,51 @@
 #include <vector>
 using namespace std;
 int n, diff = 2000;
+vector<int> used(20, 0);
+
+int getdiff(vector<vector<int>> &s)
+{
+    int a = 0, b = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (used[i]) // a팀
+        {
+            for (int j = 0; j < n; j++)
+                if (used[j])
+                    a += s[i][j];
+        }
+        else // b팀
+        {
+            for (int j = 0; j < n; j++)
+                if (!used[j])
+                    b += s[i][j];
+        }
+    }
+    return a - b > 0 ? a - b : b - a;
+}
+
+void answer(vector<vector<int>> &s, int num)
+{
+    if (n / 2 == num)
+    {
+        int ans = getdiff(s);
+        if (diff > ans)
+            diff = ans;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (!used[i])
+        {
+            used[i]++;
+            answer(s, num + 1);
+            used[i]--;
+        }
+    }
+}
 
 int main()
 {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int x;
     cin >> n;
     vector<vector<int>> s(n, vector<int>(n, 0));
@@ -16,5 +58,7 @@ int main()
             s[i][j] = x;
         }
     }
+    answer(s, 0);
+    cout << diff << '\n';
     return 0;
 }
